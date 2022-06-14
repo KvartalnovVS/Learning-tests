13ac8c6ba4c421568c9b7f3dbeed4b4b0562aa47
KvartalnovVS <kvartalnovvalera@gmail.com>
2022-06-14 18:18:46 +0300

    Add new image and JS(user)

---
+++ scripts/Beethoven.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/beethoven.jpg') {
+        myImage.setAttribute ('src','images/beethoven2.jpg');
+    } else {
+        myImage.setAttribute ('src','images/beethoven.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please, enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Beethoven is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Beethoven is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
