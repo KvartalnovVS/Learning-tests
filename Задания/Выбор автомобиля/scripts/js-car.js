6ce8b925de41690eb06c208703d940c8262eadc4
KvartalnovVS <kvartalnovvalera@gmail.com>
2022-08-22 20:00:39 +0300

    add function for choice price renge

--- Задания/Выбор автомобиля/scripts/js-car.js
+++ Задания/Выбор автомобиля/scripts/js-car.js
@@ -1,48 +1,14 @@
 //! Список моделей и их характеристики
-let superb = {
-    name: 'Superb',
-    price: 4500000,
-    color: ['white', 'black', 'cherry'],
-    maxSpeed: 280
-}
-let octavia = {
-    name: 'Octavia',
-    price: 1800000,
-    color: ['white', 'black', 'gray'],
-    maxSpeed: 150
-}
-let yeti = {
-    name: 'Yeti',
-    price: 3500000,
-    color: ['white', 'black', 'brown'],
-    maxSpeed: 150
-}
-let rapid = {
-    name: 'Rapid',
-    price: 2500000,
-    color: ['white', 'black', 'red'],
-    maxSpeed: 200
-}
+let superb = new Model('Superb', 4500000, ['white', 'black', 'cherry'], 280);
+let octavia = new Model('Octavia', 1800000, ['white', 'black', 'gray'], 150);
+let yeti = new Model('Yeti', 3500000, ['white', 'black', 'brown'], 150);
+let rapid = new Model('Rapid', 2500000, ['white', 'black', 'red'], 200);
 
-let tuareg = {
-    name: 'Tuareg',
-    price: 4000000,
-    color: ['white', 'black', 'green'],
-    maxSpeed: 260
-}
-let polo = {
-    name: 'Polo',
-    price: 1200000,
-    color: ['white', 'black', 'silver'],
-    maxSpeed: 160
-}
-let tiguan = {
-    name: 'Tiguan',
-    price: 3000000,
-    color: ['white', 'black', 'yellow'],
-    maxSpeed: 220
-}
+let tuareg = new Model('Tuareg', 4000000, ['white', 'black', 'green'], 260);
+let polo = new Model('Polo', 1200000, ['white', 'black', 'silver'], 160);
+let tiguan = new Model('Tiguan', 3000000, ['white', 'black', 'yellow'], 220);
 
+let allModels = [superb, octavia, yeti, rapid, tuareg, polo, tiguan];
 let skodaModels = [superb, octavia, yeti, rapid];
 let volkswagenModels = [tuareg, polo, tiguan];
 
@@ -56,9 +22,6 @@ let carBrandList = document.getElementById('brand-select');
 let carModelList = document.getElementById('model-select');
 let carPriceList = document.getElementById('price-select');
 
-//let modelOption = document.querySelector('.model-option');
-//modelOption.classList.remove('hidden');
-
 //! Конструктор брэнда машины и модельного ряда
 function Car(brand, models) {
     this.carBrand = brand;
@@ -66,7 +29,6 @@ function Car(brand, models) {
 }
 //? Выбор брэнда
 let brandslist = function () {
-    //let list = document.getElementById('brand-select');
     for (let i = 0; i < cars.length; i++) {
         let brand = cars[i].carBrand;
         let newBrandOption = document.createElement('option');
@@ -75,46 +37,8 @@ let brandslist = function () {
     }
 }
 brandslist();
-
-//! Конструктор каждой модели в частности
-function Model(name, price, color, maxSpeed) {
-    this.modelName = name;
-    this.modelPrice = price;
-    this.modelColor = color;
-    this.modelMaxSpeed = maxSpeed;
-}
-//? Выбор модели
-Car.prototype.modelsList = function () {
-    while (carModelList.firstChild) {
-        carModelList.removeChild(carModelList.firstChild);
-    }
-    for (let i = 0; i < this.carModels.length; i++) {
-        let model = this.carModels[i].name;
-        let newModelOption = document.createElement('option');
-        newModelOption.setAttribute('value', model);
-        newModelOption.textContent = model;
-        carModelList.appendChild(newModelOption);
-    }
-}
-
-
-function brandChange() {
-    //carModelList.removeChild();
-    for (let i = 0; i < cars.length; i++) {
-        if (carBrandList.value === cars[i].carBrand) {
-            cars[i].modelsList();
-        }
-    }
-    /*if (carBrandList.value === carSkoda.carBrand) {
-        carSkoda.modelsList();
-    } else if (carBrandList.value === carVolkswagen.carBrand) {
-        carVolkswagen.modelsList();
-    }*/
-}
-
 //? Ценовой диапозон
 function priceList() {
-    //let list = document.getElementById('price-select');
     let priceRangeMin = document.createElement('option');
     let priceRangeMid = document.createElement('option');
     let priceRangeMax = document.createElement('option');
@@ -126,3 +50,55 @@ function priceList() {
     carPriceList.appendChild(priceRangeMax);
 }
 priceList();
+
+//! Конструктор каждой модели в частности
+function Model(name, price, color, maxSpeed) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+    this.maxSpeed = maxSpeed;
+}
+//? Выбор модели
+Car.prototype.modelsList = function startModelsList() {
+    while (carModelList.firstChild) {
+        carModelList.removeChild(carModelList.firstChild);
+    }
+    for (let i = 0; i < this.carModels.length; i++) {
+        //let model = this.carModels[i].name;
+        let newModelOption = document.createElement('option');
+        let modelPrice = this.carModels[i].price;
+        newModelOption.setAttribute('value', this.carModels[i].name);
+        newModelOption.setAttribute('id', 'model-option');
+        newModelOption.textContent = this.carModels[i].name;
+        carModelList.appendChild(newModelOption);
+        if (carPriceList.value === 'до 2.000.000 руб' && modelPrice > 2000000) {
+            carModelList.removeChild(newModelOption);
+        }
+        if (carPriceList.value === 'до 3.500.000 руб' && modelPrice > 3500000) {
+            carModelList.removeChild(newModelOption);
+        }
+    }
+    /*carPriceList.onchange = function () {
+        let modelOption = document.getElementById('option');
+        if (carPriceList.value = 'до 2.000.000 руб' && Model.price > 2000000) {
+            modelOption.style.display = 'none';
+        } else if (carPriceList.value = 'до 3.500.000 руб' && Model.price > 3500000) {
+            modelOption.style.display = 'none';
+        } else if (carPriceList.value = 'до 5.000.000 руб' && Model.price > 5000000) {
+            newModelOption.style.display = 'none';
+        }
+    }*/
+}
+
+
+function brandChange() {
+    for (let i = 0; i < cars.length; i++) {
+        if (carBrandList.value === cars[i].carBrand) {
+            cars[i].modelsList();
+        }
+    }
+}
+
+carPriceList.onchange = function restart() {
+    brandChange();
+}
